6d8e9a906033db22d38c3d0b9639c92615a6ca6f
Sairi <sairi_victory-throw@yahoo.co.jp>
2020-08-25 16:16:55 +0900

    My secound commit

---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+
+  <div class="container">
+    <!-- Header -->
+
+    <div>
+      <ul>
+        <li><a href="/index"> TOP </a></li>
+        <li><a href="/about">PRODUCT</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">NEWS</a></li>
+        <li><a href="">CONTACT</a></li>
+      </ul>
+    </div>
+
+    <!-- Mainvisual -->
+    <h1>旅に出よう。</h1>
+    <div>
+      <p>
+        僕たちが作りたいのは
+        <br>
+        持っているだけで旅に出たくなるモノ。
+        <br>
+        持っているだけでわくわくするモノ。
+      </p>
+      <p>
+        それは新しい時代の"パスポート"
+        <br>
+        僕たちが作るものは、
+        <br>
+        そんな存在でありたい。
+      </p>
+      <p>
+        そして人と人とがつながる
+        <br>
+        こんな時代だからこそ、
+        <br>
+        僕たちは、みんなでひとつのモノを
+        <br>
+        作ることを追求したい。
+      </p>
+      
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: "PAS-POL -旅のモノづくりブランド-｜TABIPPO",
+      meta: [
+        { hid: 'description', name: 'description', content: 'My custom description' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+
+
+</style>
