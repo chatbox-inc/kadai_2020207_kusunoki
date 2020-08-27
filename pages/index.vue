f8ff341afa1c14eda748b446b74ad07e235797f3
Sairi <64519887+kisssairi@users.noreply.github.com>
2020-08-27 17:29:35 +0900

    Added

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
 
   <div class="container">
-    <!-- Header -->
-
-    <div>
+  <!-- Header -->
+  <header class="hearder">
+    <div class="menu">
       <ul>
         <li><a href="/index"> TOP </a></li>
         <li><a href="/about">PRODUCT</a></li>
@@ -12,8 +12,13 @@
         <li><a href="">CONTACT</a></li>
       </ul>
     </div>
+  </header>
 
-    <!-- Mainvisual -->
+  <!-- Mainvisual -->
+  <img src="image/main_visual1.jpg" alt="">
+
+  <!-- trip -->
+  <div class="trip"></div>
     <h1>旅に出よう。</h1>
     <div>
       <p>
@@ -37,12 +42,201 @@
         <br>
         僕たちは、みんなでひとつのモノを
         <br>
-        作ることを追求したい。
+        作ることを追求したい
       </p>
-      
+      </div>
 
+
+
+  <!-- product -->
+  <div class="product">
+    <h2 class="">PRODUCT</h2>
+
+    <div class="product-item1">
+        <a href="#" class="product-link">
+           <img src="#" alt="" class="product-img">
+            <p class="product-img-text">The World<br class="#"> Festival Guide<br class="#"> 海外の音楽フェス完<br class="">全ガイド</p>
+        </a>
     </div>
+
+    <div class="product-item2">
+        <a href="#" class="product-link">
+           <img src="#" alt="" class="product-img">
+            <p class="product-img-text">この世界で死ぬまで<br class="#">にしたいこと</p>
+        </a>
+    </div>
+
+    <div class="product-item3">
+        <a href="#" class="product-link">
+           <img src="#" alt="" class="product-img">
+            <p class="product-img-text">365日日本一周絶景<br class="#"> 日めくりカレンダー</p>
+        </a>
+    </div>
+
+    <div class="product-item4">
+        <a href="#" class="product-link">
+           <img src="#" alt="" class="product-img">
+            <p class="product-img-text">The World<br class="#"> Festival Guide<br class="#"> 海外の音楽フェス完全ガイド</p>
+        </a>
+    </div>
+
+    <div class="product-item5">
+        <a href="#" class="product-link">
+           <img src="#" alt="" class="product-img">
+            <p class="product-img-text">The World<br class="#"> Festival Guide<br class="#"> 海外の音楽フェス完全ガイド</p>
+        </a>
+    </div>
+
+    <div class="product-item6">
+        <a href="#" class="product-link">
+           <img src="#" alt="" class="product-img">
+            <p class="product-img-text">The World<br class="#"> Festival Guide<br class="#"> 海外の音楽フェス完全ガイド</p>
+        </a>
+    </div>
+
+
   </div>
+
+  <!-- news -->
+  <div class="news">
+    <h2 class="">NEWS</h2>
+
+    <div class="news-item1">
+      <a href="#" class="#">
+       <div class="news-box-wrap">
+         <div class="news-img-box">
+                <img src="#" alt="" class="news-img">
+         </div>
+        <div class="news-text-box">
+          <h3 class="news-title">#世界でいちばん美しい旅 を集めて、一冊の本を作ろう。私たちがいま本をつくる理由。</h3>
+          <time class="time">2020.5.26</time>
+          <p class="news-text">「旅に出たい。」この記事を読んでいる人の中には、この環境で気持ちが落ち着かない人が多いのではないでしょうか。旅が日々の楽しみだった。次の休みはどこに行こうか、なんて考える時間が大好きだった。最強のパスポートがあってもどこにも行けやしない。
+            世界中の人達が外出を制限されている中、TABIPPOができることは何なのか。
+           わたしたちは気付きました。旅に出ることができないだけで、こんなにも心が枯れてしまいそうになること。
+           わたしたちは気付きました。こんなにも旅が好きだったんだということを。だから、わ...</p>
+        </div>
+       </div>
+      </a>
+    </div>
+
+    <div class="news-item2">
+      <a href="#" class="#">
+       <div class="news-box-wrap">
+            <div class="news-img-box">
+                <img src="#" alt="" class="news-img">
+            </div>
+        <div class="news-text-box">
+        <h3 class="news-title">書籍「僕が旅人になった日」の表紙写真を募集します！</h3>
+        <time class="time">2020.5.26</time>
+          <p class="news-text">2019年5月に制作がスタートした書籍の完成がようやく見えてきました。
+            旅人のエッセイを集めた書籍。「旅人になった日」にフォーカスをあてて、様々な旅のエピソードをnoteとコラボして募集をしました。
+            noteで、#旅とわたし　のハッシュタグをつけてもらい募集した企画です。4300件ほどの旅の話が集まり、私たちもたくさんの応募に驚きと嬉しでいっぱいでした。
+            ご応募いただいたみなさんありがとうございました！最終的に20人のエッセイとさらに追加で募集をしたお写真を載せた1冊になります。
+            中身をチラ見せ...</p>
+        </div>
+       </div>
+      </a>
+    </div>
+
+    <div class="news-item3">
+      <a href="#" class="#">
+       <div class="news-box-wrap">
+            <div class="news-img-box">
+                <img src="#" alt="" class="news-img">
+            </div>
+        <div class="news-text-box">
+        <h3 class="news-title">「BUCKET LIST/人生でしたい100のことを書くノート」に、これからしたいことを書き留めませんか？</h3>
+        <time class="time">2020.5.26</time>
+          <p class="news-text">こんにちは。TABIPPOの中美砂希です。
+            外出が制限され、みなさんご自宅でそれぞれの時間を過ごされていることと思います。
+            今の時間にしかできないこと、いつか何も気にすることなく外に出られた時、旅に行けるときがやってくるその時を楽しみにして。
+            「BUCKET LIST/人生でしたい100のことを書くノート」にこれからしたいことを書き留めてみませんか？
+            BUCKET LIST/人生でしたい100のことを書くノート 出版社: いろは出版 価格：1,800円（税別）リニューアルしたBUC...</p>
+        </div>
+       </div>
+      </a>
+    </div>
+
+    <div class="news-item4">
+      <a href="#" class="#">
+       <div class="news-box-wrap">
+            <div class="news-img-box">
+                <img src="#" alt="" class="news-img">
+            </div>
+        <div class="news-text-box">
+        <h3 class="news-title">新書籍「僕が旅人になった日」のアンケートにご協力ください！</h3>
+        <time class="time">2020.5.26</time>
+          <p class="news-text">2019年5月に制作がスタートした書籍の完成がようやく見えてきました。
+            旅人のエッセイを集めた書籍。「旅人になった日」にフォーカスをあてて、様々な旅のエピソードをnoteとコラボして募集をしました。
+            noteで、#旅とわたし　のハッシュタグをつけてもらい募集した企画です。4300件ほどの旅の話が集まり、私たちもたくさんの応募に驚きと嬉しでいっぱいでした。
+            ご応募いただいたみなさんありがとうございました！最終的に20人のエッセイとさらに追加で募集をしたお写真を載せた1冊になります。
+            中身をチラ見せ...</p>
+        </div>
+       </div>
+      </a>
+    </div>
+
+    <div class="news-item5">
+      <a href="#" class="#">
+       <div class="news-box-wrap">
+            <div class="news-img-box">
+                <img src="#" alt="" class="news-img">
+            </div>
+        <div class="news-text-box">
+        <h3 class="news-title">新書籍「旅人になった日」に掲載される絶景の写真を大募集！</h3>
+        <time class="time">2020.5.26</time>
+          <p class="news-text">2019年5月に制作がスタートした書籍の完成がようやく見えてきました。
+            旅人のエッセイを集めた書籍。「旅人になった日」にフォーカスをあてて、様々な旅のエピソードをnoteとコラボして募集をしました。
+            noteで、#旅とわたし　のハッシュタグをつけてもらい募集した企画です。4300件ほどの旅の話が集まり、私たちもたくさんの応募に驚きと嬉しでいっぱいでした。
+            ご応募いただいたみなさんありがとうございました！最終的に20人のエッセイとさらに追加で募集をしたお写真を載せた1冊になります。
+            中身をチラ見せ...</p>
+        </div>
+       </div>
+      </a>
+    </div>
+    
+    <div class="news-item6">
+      <a href="#" class="#">
+       <div class="news-box-wrap">
+            <div class="news-img-box">
+                <img src="#" alt="" class="news-img">
+            </div>
+        <div class="news-text-box">
+        <h3 class="news-title">noteコラボ企画のお題「#旅とわたし」の投稿が3000件突破！！</h3>
+        <time class="time">2020.5.26</time>
+          <p class="news-text">2019年5月に制作がスタートした書籍の完成がようやく見えてきました。
+            旅人のエッセイを集めた書籍。「旅人になった日」にフォーカスをあてて、様々な旅のエピソードをnoteとコラボして募集をしました。
+            noteで、#旅とわたし　のハッシュタグをつけてもらい募集した企画です。4300件ほどの旅の話が集まり、私たちもたくさんの応募に驚きと嬉しでいっぱいでした。
+            ご応募いただいたみなさんありがとうございました！最終的に20人のエッセイとさらに追加で募集をしたお写真を載せた1冊になります。
+            中身をチラ見せ...</p>
+        </div>
+       </div>
+      </a>
+    </div>
+
+
+  </div>
+
+  <!-- sns -->
+  <div class="sns">
+
+    <p class="sns-text">Share on facebook</p>
+    <p class="sns-text">Share on Twitter</p>
+    <p class="sns-text">Hatena Bookmark</p>
+
+  </div>
+  <!-- footer -->
+  <footer class="footer">
+    <p class="copyright">
+      Copyright © 2018 PAS-POL -旅のモノづくりブランド-｜TABIPPO All rights reserved.
+    </p>
+
+
+  </footer>
+    
+
+
+</div>
 </template>
 
 <script>
@@ -59,7 +253,14 @@ export default {
 </script>
 
 <style>
+.menu li{
+  display: inline;
+}
 
 
+
+.sns-text {
+  display: inline;
+}
 
 </style>
